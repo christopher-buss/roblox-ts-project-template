65084238a23103841e4f24e75e76bbbef6b404eb
christopher-buss <christopher.buss+github@protonmail.com>
2024-06-18 22:27:18 +0100

    feat: add asphalt support (#60)

---
+++ src/shared/assets.d.ts
@@ -0,0 +1,10 @@
+declare const assets: {
+	badges: {
+		"example-badge": "rbxassetid://18110928024";
+	};
+	marketing: {
+		"example-icon": "rbxassetid://18110928241";
+		googly: "rbxassetid://18110928530";
+	};
+};
+export = assets;
