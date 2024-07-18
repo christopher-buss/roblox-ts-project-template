db0cc5a25d4212ff8e676e5050e2cdf696b2ddbe
christopher-buss <christopher.buss+github@protonmail.com>
2024-07-18 20:45:22 +0100

    style(lint): fix eslint error

--- src/client/ui/components/error-handler/error-page.tsx
+++ src/client/ui/components/error-handler/error-page.tsx
@@ -7,5 +7,6 @@ interface ErrorPageProps {
 export default function ErrorPage({ Message }: ErrorPageProps): React.Element {
 	warn(Message);
 
+	// eslint-disable-next-line react/no-useless-fragment -- This is a placeholder for future functions.
 	return <></>;
 }
