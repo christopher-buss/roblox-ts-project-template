76b5e4c95d1699b43f7c3a2bfccb80437cbf510f
christopher-buss <christopher.buss+github@protonmail.com>
2025-09-15 16:58:51 +0100

    build: update @isentinel/eslint-config to version 3.0.0-beta.8
    Remove temporary action sorting from config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,4 +1,4 @@
-import isentinel, { GLOB_SRC, GLOB_YAML } from "@isentinel/eslint-config";
+import isentinel, { GLOB_SRC } from "@isentinel/eslint-config";
 
 export default isentinel(
 	{
@@ -32,62 +32,10 @@ export default isentinel(
 						reactProps: ["^children$", "^ref$"],
 						reflex: ["^loadPlayerData$", "^closePlayerData$"],
 					},
-					groups: ["id", "name", "reflex", "unknown", "reactProps"],
+					groups: ["id", "name", "reflex", "unknown", "reactProps", "callbacks"],
 					order: "asc",
 					partitionByComment: "^Part:\\*\\*(.*)$",
 					type: "natural",
-				},
-			],
-		},
-	},
-	{
-		name: "isentinel/sort/github-composite-actions",
-		files: [`.github/actions/${GLOB_YAML}`],
-		rules: {
-			"yaml/sort-keys": [
-				"error",
-				// Composite action root
-				{
-					order: ["name", "description", "inputs", "outputs", "runs"],
-					pathPattern: "^$",
-				},
-				// Composite inputs
-				{
-					order: ["description", "required", "default"],
-					pathPattern: "^inputs\\.[^.]+$",
-				},
-				// Composite outputs
-				{
-					order: ["description", "value"],
-					pathPattern: "^outputs\\.[^.]+$",
-				},
-				// Composite runs
-				{
-					order: ["using", "steps"],
-					pathPattern: "^runs$",
-				},
-				// Composite steps
-				{
-					order: [
-						"id",
-						"if",
-						"name",
-						"uses",
-						"run",
-						"working-directory",
-						"with",
-						"env",
-						"shell",
-						"continue-on-error",
-						"timeout-minutes",
-					],
-					pathPattern: "^runs\\.steps\\[\\d+\\]$",
-				},
-				// General nested key sorting for everything else
-				{
-					order: { type: "asc" },
-					pathPattern:
-						"^(?!^$|inputs\\.[^.]+$|outputs\\.[^.]+$|runs$|runs\\.steps\\[\\d+\\]$).*$",
 				},
 			],
 		},
