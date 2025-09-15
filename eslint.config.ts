1d5c35b2a540c10db7e8b1f1ccb796f97910d005
christopher-buss <christopher.buss+github@protonmail.com>
2025-09-15 18:55:31 +0100

    style(deps): update eslint-config to v3.0.0

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,43 +1,29 @@
-import isentinel, { GLOB_SRC } from "@isentinel/eslint-config";
+import isentinel from "@isentinel/eslint-config";
 
-export default isentinel(
-	{
-		name: "project/base",
-		perfectionist: {
-			customClassGroups: [
-				"onInit",
-				"onStart",
-				"onPlayerJoin",
-				"onPlayerLeave",
-				"onRender",
-				"onPhysics",
-				"onTick",
-			],
-		},
-		pnpm: true,
-		react: true,
-		type: "game",
-	},
-	{
-		name: "project/sort",
-		files: [GLOB_SRC],
-		rules: {
-			"perfectionist/sort-objects": [
-				"error",
-				{
-					customGroups: {
-						id: "^id$",
-						name: "^name$",
-						callbacks: ["\b(on[A-Z][a-zA-Z]*)\b"],
-						reactProps: ["^children$", "^ref$"],
-						reflex: ["^loadPlayerData$", "^closePlayerData$"],
-					},
-					groups: ["id", "name", "reflex", "unknown", "reactProps", "callbacks"],
-					order: "asc",
-					partitionByComment: "^Part:\\*\\*(.*)$",
-					type: "natural",
-				},
-			],
+export default isentinel({
+	name: "project/base",
+	perfectionist: {
+		customClassGroups: [
+			"onInit",
+			"onStart",
+			"onPlayerJoin",
+			"onPlayerLeave",
+			"onRender",
+			"onPhysics",
+			"onTick",
+		],
+		sortObjects: {
+			customGroups: {
+				id: "^id$",
+				name: "^name$",
+				callbacks: ["\b(on[A-Z][a-zA-Z]*)\b"],
+				reactProps: ["^children$", "^ref$"],
+				reflex: ["^loadPlayerData$", "^closePlayerData$"],
+			},
+			groups: ["id", "name", "reflex", "unknown", "reactProps", "callbacks"],
 		},
 	},
-);
+	pnpm: true,
+	react: true,
+	type: "game",
+});
